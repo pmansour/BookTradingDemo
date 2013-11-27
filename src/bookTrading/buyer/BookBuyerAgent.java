e040e374e8c223d78bf3fef72740de9403fb499b
Peter Mansour <pmansour112@gmail.com>
2013-11-27 17:13:07 +1100

    added initial buyer agent

---
+++ src/bookTrading/buyer/BookBuyerAgent.java
@@ -0,0 +1,17 @@
+package bookTrading.buyer;
+
+import jade.core.Agent;
+
+public class BookBuyerAgent extends Agent {
+	private static final long serialVersionUID = -2179361359046163266L;
+
+	@Override
+	protected void setup() {
+		
+	}
+	
+	@Override
+	protected void takeDown() {
+		
+	}
+}
