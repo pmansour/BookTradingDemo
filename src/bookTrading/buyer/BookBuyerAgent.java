653c547cb274109e08b5018bde67a5b3b973036c
Peter Mansour <pmansour112@gmail.com>
2013-11-27 17:31:29 +1100

    wrote skeleton for book buyer agent and gui

--- src/bookTrading/buyer/BookBuyerAgent.java
+++ src/bookTrading/buyer/BookBuyerAgent.java
@@ -1,17 +1,48 @@
 package bookTrading.buyer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
 import jade.core.Agent;
 
 public class BookBuyerAgent extends Agent {
 	private static final long serialVersionUID = -2179361359046163266L;
 
+	// the agents that are selling books
+	private List<AID> sellers;
+	// the GUI that shows the state of this buyer
+	private BookBuyerGUI gui;
+	
 	@Override
 	protected void setup() {
+		// print a welcome message
+		System.out.println("BBA " + getAID().getName() + " ready.");
 		
+		// load the sellers from the command-line arguments
+		Object[] arguments = getArguments();
+		if(arguments != null) {
+			sellers = new ArrayList<AID>(arguments.length);
+			for(Object seller : arguments) {
+				// create the sellers' AIDs from their local names
+				sellers.add(new AID((String) seller, AID.ISLOCALNAME));
+			}
+		}
+		
+		// show the GUI
+		gui = new BookBuyerGUI(this);
+		gui.show();
 	}
 	
 	@Override
 	protected void takeDown() {
+		// dispose of the GUI if needed
+		if(gui != null) {
+			gui.dispose();
+			gui = null;
+		}
 		
+		// print a goodbye message
+		System.out.println("BBA " + getAID().getName() + " terminated.");
 	}
 }
---
+++ src/bookTrading/buyer/BookBuyerGUI.java
@@ -0,0 +1,23 @@
+package bookTrading.buyer;
+
+/**
+ * The GUI that will be used to show the status of a book-buyer agent.
+ * @author peter
+ *
+ */
+public class BookBuyerGUI {
+
+	private BookBuyerAgent agent;
+	
+	public BookBuyerGUI(BookBuyerAgent agent) {
+		this.agent = agent;
+	}
+	
+	public void show() {
+		//TODO
+	}
+	
+	public void dispose() {
+		//TODO
+	}
+}
